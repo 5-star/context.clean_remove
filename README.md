12a3294811f66fe864df984c4ab8f806a8ca805a
Luis Rodrigues <luis@rodrigues.pt>
2016-06-09 15:50:52 +0100

    Initial Release

---
+++ README.md
@@ -0,0 +1,30 @@
+plugin.video.LR.clean_remove
+----------------------------
+
+A Kodi add-on that delete a movie or episode or video cleanly, withou leaving behind orphan files.
+
+I have been using this for quite a while without any problems, so I am sharing it for public use. USE IT AT YOUR OWN RISK.
+It will work well only if you kept the default naming terminology and paths for fanart and subtitles. Please see below for more details.
+
+WARNING!!!
+Only use this addon if you have one movie on separate folders. When you delete one movie it will remove the movie from library and then delete the folder where the movie is, this way deleting all files related to that movie.
+If you have all the movies in one single folder, deleting one movie will delete them all.
+
+Deleting a tvshow episode will remove it from the library and then delete the episode file and related files withe the same 'root' name.
+Example: Deleting an episode with a filename abc.mp4, will delete all files that start with 'abc' on the folder where the episode is.
+This way it will delete abc.mp4, abc.nfo, abc-thumb.jpg and abc.srt.
+If it was the last episode on the season and folder, it will delete the folder if it's empty.
+
+For videos not in the library, it will just delete the video and related fanart that have the same 'root' name, just like the tvshow episodes.
+
+Installation
+------------
+
+ - Download the add-on as a ZIP file from the top of this page
+ - Open Kodi
+ - Go to `System -> Settings -> Add-ons -> Install from zip file`
+ - Restart Kodi and enjoy :)
+ 
+Release history
+---------------
+  * 2016-06-10 v 1.0.1 Initial release
---
+++ addon.py
@@ -0,0 +1,67 @@
+import os
+import glob
+import sys
+import json
+import xbmc
+import xbmcgui
+
+lang = addon.getLocalizedString
+
+def kodiJsonRequest(params):
+    data = json.dumps(params)
+    request = xbmc.executeJSONRPC(data)
+    try:
+        response = json.loads(request)
+    except UnicodeDecodeError:
+        response = json.loads(request.decode('utf-8', 'ignore'))
+    try:
+        if 'result' in response:
+            return response['result']
+        return None
+    except KeyError:
+        logger.warn("[%s] %s" % (params['method'], response['error']['message']))
+        return None
+
+
+def trataMovie():
+    for fl in glob.glob(path + "*.*"): os.remove(fl)
+    try: os.rmdir(path)
+    except: logger.warn('not empty, keep folder')
+    kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveMovie', 'params': {'movieid': int(id)}, 'id': 1})
+    xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30002))
+
+def trataEpisode():
+    base = os.path.splitext(xbmc.getInfoLabel('ListItem.FileName'))[0]
+    kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveEpisode', 'params': {'episodeid': int(id)}, 'id': 1})
+    for fl in glob.glob(path + base + "*.*"): os.remove(fl)
+    try: os.rmdir(path)
+    except: logger.warn('not empty, keep folder')
+    xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30003))
+
+def trataMusicVideos():
+    base = os.path.splitext(xbmc.getInfoLabel('ListItem.FileName'))[0]
+    kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveMusicVideo', 'params': {'musicvideoid': int(id)}, 'id': 1})
+    for fl in glob.glob(path + base + "*.*"): os.remove(fl)
+    try: os.rmdir(path)
+    except: logger.warn('not empty, keep folder')
+    xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30004))
+
+def trataVideos():
+    base = os.path.splitext(xbmc.getInfoLabel('ListItem.FileName'))[0]
+    for fl in glob.glob(path + base + "*.*"): os.remove(fl)
+    xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30005))
+
+def trata():
+    id=xbmc.getInfoLabel('ListItem.DBID')
+    if id=='-1':
+        id=xbmc.getInfoLabel('ListItem.title')
+    path=xbmc.getInfoLabel('ListItem.Path').replace('smb:','')
+    if xbmcgui.Dialog().yesno(lang(30006),xbmc.getInfoLabel('ListItem.Label')):
+        if xbmc.getInfoLabel('Container.Content')=='movies': trataMovie()
+        if xbmc.getInfoLabel('Container.Content')=='episodes': trataEpisode()
+        if xbmc.getInfoLabel('Container.Content')=='musicvideos': trataMusicVideos()
+        if xbmc.getInfoLabel('Container.Content')=='files': trataVideos()
+ 
+if __name__ == '__main__':
+    if xbmc.getInfoLabel('ListItem.FileName')!="":
+        trata()
