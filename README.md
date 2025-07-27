15d0ecac870343354aac8bfcf21c112a2ff0c4eb
Remigijus Gaigalas <102466590+Myginas@users.noreply.github.com>
2025-07-27 16:29:06 +0300

    Fix
    Fix: Remove non empty folders.
    Add: Container update path and refresh.
    Add: Lithuanian translation.
    Add: Remove folder which name start same as video file name.
    Remove separate_movies setting
    Simplified code.

--- README.md
+++ README.md
@@ -3,31 +3,18 @@ context.clean_remove
 
 A Kodi add-on to delete a movie or episode or video cleanly, without leaving behind orphan files.
 
-The kodi "Remove from library" is a nice feature, but only deletes the movie being played. After I started using it, I noticed that I had to manually delete the fanart, the nfo and the subtitles. Or instead ending up with a disk full of unsused garbish.  
+The kodi "Remove from library" is a nice feature, but only deletes the movie being played. After I started using it, I noticed that I had to manually delete the fanart, the nfo, subtitles, .actors, .trailers etc. Or instead ending up with a disk full of unused garbish.
 So I came upo with this clean remove, a library removal tool that removes the media and all related files.
 
 I have been using this for quite a while without any problems, so I am sharing it for public use. USE IT AT YOUR OWN RISK.
-It will work well only if you kept the default naming terminology and paths for fanart and subtitles. Please see below for more details.
+It will work well only if you kept the default naming terminology and paths for fanart and subtitles.
 
 WARNING!!!
-Pay attention to the setting "Movies in separate folders". If this is true, when you delete one movie it will remove the movie from the library and then delete the folder where the movie is, this way deleting all files related to that movie.
-If you have all the movies in one single folder and set the 'Movies in separate folders', deleting one movie will delete them all. Use this option only if you have movies on separate folders, one movie per folder. 
+When you delete folder it will remove folder but not entries from library.
 
-For all other media types (episodes, music videos and videos) this will only delete the fanart, nfo and subtitles related to the media being removed form the library. Everything else on the same folder is not touched.
+For  media types (movies, episodes, music videos and videos) this will delete the files with names that start same as media file name fanart, nfo and subtitles related to the media being removed form the library. Everything else on the same folder is not touched. Will delete folder with same name as file name. 
 
 Optionally, if it was the last episode on the season/folder, it will delete the folder if it's empty.
-
-Files with the following extensions and the same name as the movie/episode are deleted:
-".srt"
-".pt.srt"
-".en.srt"
-".nfo"
-".jpg"
-"-poster.jpg"
-"-fanart.jpg"
-"-thumb.jpg"
-"-banner.jpg"
-"-thumb.jpg"
 
 Installation
 ------------
@@ -39,6 +26,7 @@ Installation
  
 Release history
 ---------------
+  * 7.0.0 Fix
   * 6.0.0 Kodi 19
   * 1.0.2 Update to Kodi 18
   * 1.0.1 Initial release
--- addon.py
+++ addon.py
@@ -7,97 +7,80 @@ import xbmc
 import xbmcgui
 import xbmcaddon
 import xbmcvfs
-import re
 
 addon = xbmcaddon.Addon()
 lang = addon.getLocalizedString
-
-separate_movies = addon.getSetting('separate_movies')
+path=xbmc.getInfoLabel('ListItem.Path')
 remove_empty = addon.getSetting('remove_empty')
 
 def kodiJsonRequest(params):
-    data = json.dumps(params)
-    request = xbmc.executeJSONRPC(data)
-    try:
-        response = json.loads(request)
-    except UnicodeDecodeError:
-        response = json.loads(request.decode('utf-8', 'ignore'))
-    try:
-        if 'result' in response:
-            return response['result']
-        return None
-    except KeyError:
-        logger.warn("[%s] %s" % (params['method'], response['error']['message']))
-        return None
-
-def deleteFile(file):
-    if xbmcvfs.exists(file):
-        xbmcvfs.delete(file)
+	data = json.dumps(params)
+	request = xbmc.executeJSONRPC(data)
+	try:
+		response = json.loads(request)
+	except UnicodeDecodeError:
+		response = json.loads(request.decode('utf-8', 'ignore'))
+	try:
+		if 'result' in response:
+			return response['result']
+		return None
+	except KeyError:
+		logger.warn("[%s] %s" % (params['method'], response['error']['message']))
+		return None
 
 def deleteDir(dir):
-    if xbmcvfs.exists(dir):
-        files=xbmcvfs.listdir(dir)
-        if len(files)==2:
-            xbmcvfs.rmdir(dir)
+	global path
+	path = os.path.dirname(os.path.dirname(path))
+	# In file browser open the parent folder.
+	if dir==xbmc.getInfoLabel('Container.FolderPath'):
+		xbmc.executebuiltin(f'Container.Update("{path}")')
 
-def deleteVideo(path, video):
-    deleteFile(path + video)
-    filename = os.path.splitext(video)[0]
-    filebase = path + filename
-    xdir, xfil = xbmcvfs.listdir(path)
-    for fl in xfil:
-        if re.match(re.escape(filename) + "\.[a-z]{2}\.srt$", fl):
-            xbmcvfs.delete(path + fl)
-    deleteFile(filebase + ".srt")
-    deleteFile(filebase + ".nfo")
-    deleteFile(filebase + ".jpg")
-    deleteFile(filebase + "-poster.jpg")
-    deleteFile(filebase + "-fanart.jpg")
-    deleteFile(filebase + "-thumb.jpg")
-    deleteFile(filebase + "-banner.jpg")
-    deleteFile(filebase + "-thumb.jpg")
-    if remove_empty == "true":
-        deleteDir(path)
+	xbmcvfs.rmdir(dir, force=True)
 
-def dltMovie(id, path, video):
-    kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveMovie', 'params': {'movieid': int(id)}, 'id': 1})
-    if separate_movies == "true":
-        xdir, xfil = xbmcvfs.listdir(path)
-        for fl in xfil:
-            xbmcvfs.delete(path + fl)
-        deleteDir(path)
-    else:
-        deleteVideo(path, video)
-    xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30002) + ')')
-
-def dltEpisode(id, path, video):
-    kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveEpisode', 'params': {'episodeid': int(id)}, 'id': 1})
-    deleteVideo(path, video)
-    xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30003) + ')')
-
-def dltMusicVideos(id, path, video):
-    kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveMusicVideo', 'params': {'musicvideoid': int(id)}, 'id': 1})
-    deleteVideo(path, video)
-    xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30004) + ')')
-
-def dltVideos(path, video):
-    deleteVideo(path, video)
-    xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30005) + ')')
+def deleteFiles(video):
+	global path
+	filename = os.path.splitext(video)[0] # File name without extension.
+	# Delete parent folder if it name same as file name.
+	if os.path.basename(os.path.dirname(path)) == filename:
+		deleteDir(path)
+	else:
+		# Remove files which names start same as video file name.
+		pattern = os.path.join(path, filename + '*')
+		files = glob.glob(pattern)
+		for file in files:
+			xbmcvfs.delete(file)
 
 def dlt():
-    if xbmcgui.Dialog().yesno(lang(30006),xbmc.getInfoLabel('ListItem.Label')):
-        path=xbmc.getInfoLabel('ListItem.Path')
-        video = xbmc.getInfoLabel('ListItem.FileName')
-        if xbmc.getInfoLabel('Container.Content')=='files':
-            dltVideos(path, video)
-        else:
-            id=int(xbmc.getInfoLabel('ListItem.DBID'))
-            if id<0: id=int(xbmc.getInfoLabel('ListItem.Top250'));
-            if id>0:
-                dbtype = xbmc.getInfoLabel('ListItem.DBTYPE')
-                if dbtype=='movie': dltMovie(id, path, video)
-                elif dbtype=='episode': dltEpisode(id, path, video)
-                elif dbtype=='musicvideo': dltMusicVideos(id, path, video)
- 
+	video=xbmc.getInfoLabel('ListItem.FileName')
+	if xbmcgui.Dialog().yesno(lang(30006),xbmc.getInfoLabel('ListItem.Label')):
+		if xbmc.getInfoLabel('Container.Content')=='files':
+			xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30005) + ')')
+		else:
+			id=int(xbmc.getInfoLabel('ListItem.DBID'))
+			if id<0: id=int(xbmc.getInfoLabel('ListItem.Top250'));
+			if id>0:
+				dbtype = xbmc.getInfoLabel('ListItem.DBTYPE')
+				if dbtype=='movie':
+					kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveMovie', 'params': {'movieid': int(id)}, 'id': 1})
+					xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30002) + ')')
+				elif dbtype=='episode':
+					kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveEpisode', 'params': {'episodeid': int(id)}, 'id': 1})
+					xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30003) + ')')
+				elif dbtype=='musicvideo':
+					kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveMusicVideo', 'params': {'musicvideoid': int(id)}, 'id': 1})
+					xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30004) + ')')
+
+		if video!='':
+			deleteFiles(video)
+		else:
+			deleteDir(xbmc.getInfoLabel('Container.ListItem.FileNameAndPath'))
+
+		# Remove empty folder.
+		if remove_empty == "true":
+			if len(os.listdir(path))==0:
+				deleteDir(path)
+
+	xbmc.executebuiltin('Container.Refresh')
+
 if __name__ == '__main__':
-    dlt()
+	dlt()
