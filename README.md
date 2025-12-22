8753464f5d59492f6c4cc2141152486169eed2cd
Remigijus Gaigalas <102466590+Myginas@users.noreply.github.com>
2025-12-22 19:42:23 +0200

    Add feature delete form favourites.
    Kodi does not provide any API / builtin or JSON-RPC method to remove favourite item. Firstly use "Clean remove" context menu item to remove files / folders and database entries, then remove empty favourite item using kodi context menu item "Remove".

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ context.clean_remove
 A Kodi add-on to delete a movie or episode or video cleanly, without leaving behind orphan files.
 
 The kodi "Remove from library" is a nice feature, but only deletes the movie being played. After I started using it, I noticed that I had to manually delete the fanart, the nfo, subtitles, .actors, .trailers etc. Or instead ending up with a disk full of unused garbish.
-So I came upo with this clean remove, a library removal tool that removes the media and all related files.
+So I came up with this clean remove, a library removal tool that removes the media and all related files.
 
 I have been using this for quite a while without any problems, so I am sharing it for public use. USE IT AT YOUR OWN RISK.
 It will work well only if you kept the default naming terminology and paths for fanart and subtitles.
 
-For media types such as movies, episodes, music videos, and videos, this will remove files whose names begin with the same prefix as the media file. Other files or folders in the same directory will remain unaffected. But if folder name match the file name then deletes folder.
+For media types such as movies, episodes, music videos, and videos, this will remove files whose names begin same as the media file. Other files or folders in the same directory will remain unaffected. But if folder name match the file name then deletes folder.
+
+Kodi does not provide any API / builtin or JSON-RPC method to remove favourite item. Firstly use "Clean remove" context menu item to remove files / folders and database entries, then remove empty favourite item using kodi context menu item "Remove".
 
 Optionally, if it was the last episode on the season/folder, it will delete the folder if it's empty.
 
@@ -23,6 +25,7 @@ Installation
  
 Release history
 ---------------
+  * 7.3.0 Add delete form favourites.
   * 7.2.0 Fix lithuanian translation. Add confirm setting.
   * 7.1.0 Fix open the parent folder. Fix delete empty folder.
   * 7.0.0 Fix. Add Lithuanian translation.
--- addon.py
+++ addon.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 import os
 import glob
-import sys
 import json
 import xbmc
 import xbmcgui
 import xbmcaddon
 import xbmcvfs
+from urllib.parse import unquote
 
 addon = xbmcaddon.Addon()
-lang = addon.getLocalizedString
+language = addon.getLocalizedString
 path=xbmc.getInfoLabel('ListItem.Path')
 remove_empty_folder = addon.getSetting('remove_empty_folder')
 confirm = addon.getSetting('confirm')
@@ -54,33 +54,51 @@ def deleteFiles(video):
 				deleteDir(path)
 
 def cleanRemove():
-	video=xbmc.getInfoLabel('ListItem.FileName')
-	if video!='':
-		deleteFiles(video)
+	global path
+	database_data=None
+	if path=='favourites://':
+		path=unquote(xbmc.getInfoLabel('ListItem.FileNameAndPath')).split('"')[1]
+		database_data=kodiJsonRequest({"jsonrpc": "2.0", "method": "Files.GetFileDetails", "params": { "file": path, "media": "video"}, "id": 1 })
+		database_data=database_data.get("filedetails", {})
+		video=os.path.basename(path)
+		path=os.path.dirname(path) + os.sep
 	else:
-		deleteDir(xbmc.getInfoLabel('Container.ListItem.FileNameAndPath'))
+		video=xbmc.getInfoLabel('ListItem.FileName')
 
 	if xbmc.getInfoLabel('Container.Content')=='files':
-		xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30005) + ')')
+		xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + language(30005) + ')')
 	else:
-		id=int(xbmc.getInfoLabel('ListItem.DBID'))
+		if not database_data:
+			id=int(xbmc.getInfoLabel('ListItem.DBID'))
+		else:
+			id=int(database_data["id"])
+
 		if id<0: id=int(xbmc.getInfoLabel('ListItem.Top250'));
 		if id>0:
-			dbtype = xbmc.getInfoLabel('ListItem.DBTYPE')
+			if not database_data:
+				dbtype = xbmc.getInfoLabel('ListItem.DBTYPE')
+			else:
+				dbtype=database_data["type"]
+
 			if dbtype=='movie':
 				kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveMovie', 'params': {'movieid': int(id)}, 'id': 1})
-				xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30002) + ')')
+				xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + language(30002) + ')')
 			elif dbtype=='episode':
 				kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveEpisode', 'params': {'episodeid': int(id)}, 'id': 1})
-				xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30003) + ')')
+				xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + language(30003) + ')')
 			elif dbtype=='musicvideo':
 				kodiJsonRequest({'jsonrpc': '2.0', 'method': 'VideoLibrary.RemoveMusicVideo', 'params': {'musicvideoid': int(id)}, 'id': 1})
-				xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + lang(30004) + ')')
+				xbmc.executebuiltin('Notification(' + xbmc.getInfoLabel('ListItem.Label').replace(",",";") + ',' + language(30004) + ')')
+
+	if video!='':
+		deleteFiles(video) 
+	else:
+		deleteDir(xbmc.getInfoLabel('Container.ListItem.FileNameAndPath'))
 
 	xbmc.executebuiltin('Container.Refresh')
 
 if confirm == "true":
-	if xbmcgui.Dialog().yesno(lang(30006), xbmc.getInfoLabel('ListItem.Label')):
+	if xbmcgui.Dialog().yesno(language(30006), xbmc.getInfoLabel('ListItem.Label')):
 		cleanRemove()
 else:
 	cleanRemove()
